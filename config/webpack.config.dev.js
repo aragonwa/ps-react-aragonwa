dfcbbcf0abd51430a7dff7e54e7b558e0fb0d8bc
Alex Aragon <alexaragon@ACA8006B.ipt.aol.com>
2017-09-05 19:08:37 -0700

    Add alias to components folder

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -86,10 +86,10 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'ps-react': path.resolve(__dirname, '../src/components')
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
