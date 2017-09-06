132f2dee709df3f4f82aa93380d853e0272ceb58
Alex Aragon <alexaragon@ACA8006B.ipt.aol.com>
2017-09-05 19:10:01 -0700

    Refactoring import stmt && adding index file

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Custom Message */
+export default function ExampleHelloWorl(){
+  return <HelloWorld message="Pluralsight viewers!" />
+}
